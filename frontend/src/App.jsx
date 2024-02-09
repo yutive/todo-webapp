abe5362203947e6613f85cab711bec606c254878
M0608766 <laurin.schwander@mgb.ch>
2024-02-09 16:18:47 +0100

    added delete endpoint to the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 
+
+
 const App = () => {
     const [todos, setTodos] = useState([]);
     const [newTodo, setNewTodo] = useState('');
@@ -39,7 +41,6 @@ const App = () => {
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
-                // After submitting the new todo, fetch the updated list of todos
                 fetchTodos();
             })
             .catch((error) => {
@@ -49,9 +50,25 @@ const App = () => {
         setNewTodo(''); // Clear the input field
     };
 
+    function handleDelete(id) {
+        fetch(`http://localhost:8080/api/v1/delete/${id}`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP status " + response.status);
+                }
+                return response.json();
+            })
+            .then(() => {
+                fetchTodos();
+            })
+            .catch(error => console.error('An error occurred:', error));
+    }
 
     return (
-        <div className="min-h-screen bg-gradient-to-r from-blue-500 to-fuchsia-400 flex flex-col items-center justify-center text-white font-poppins px-4 sm:px-0">
+        <div
+            className="min-h-screen bg-gradient-to-r from-blue-500 to-fuchsia-400 flex flex-col items-center justify-center text-white font-poppins px-4 sm:px-0">
             <h1 className="text-5xl font-bold mb-8 text-center">Todo List</h1>
             <form onSubmit={handleNewTodoSubmit} className="w-full max-w-md mb-8">
                 <input
@@ -61,11 +78,26 @@ const App = () => {
                     placeholder="New Todo"
                     className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent text-black"
                 />
-                <button type="submit" className="w-full mt-2 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md shadow-sm">Add Todo</button>
+                <button type="submit"
+                        className="w-full mt-2 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md shadow-sm">Add
+                    Todo
+                </button>
             </form>
             <ul className="w-full max-w-md bg-white rounded-xl shadow-md p-6 text-black">
                 {todos && todos.map(todo => (
-                    todo.text && <li key={todo.id} className="text-lg my-2 border-b-2 border-gray-200 py-2">{todo.text}</li>
+                    todo.text &&
+                    <li key={todo.id}
+                        className="flex justify-between items-center my-2 border-b-2 border-gray-200 py-2">
+                        <p>
+                            {todo.text}
+                        </p>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                            onClick={() => handleDelete(todo.id)}
+                        >
+                            X
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
