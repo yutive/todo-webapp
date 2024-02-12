d2808539b770e098e4b5587e89f1a6e36d8682cc
M0608766 <laurin.schwander@mgb.ch>
2024-02-12 08:21:11 +0100

    changed styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,8 +80,8 @@ const App = () => {
                     className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent text-black"
                 />
                 <button type="submit"
-                        className="w-full mt-2 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md shadow-sm">Add
-                    Todo
+                        className="w-full mt-2 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md shadow-sm">
+                    Add Todo
                 </button>
             </form>
             <ul className="w-full max-w-md bg-white rounded-xl shadow-md p-6 text-black">
